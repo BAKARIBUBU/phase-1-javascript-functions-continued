eed0056110dd5f33865d4208dfb2c81781c27ffa
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-26 09:13:04 +0300

    function continuation

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// index.js
+
+// saturdayFun function declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// wrapAdjective function
+function wrapAdjective(wrapper) {
+  return function(adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  }
+}
+
+// Exporting functions if needed
+module.exports = { saturdayFun, mondayWork, wrapAdjective };
